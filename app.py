7e6c3f944ceb981923e206ac3fb14ea029e81add
Yujin Hwang <skrtkd0416@naver.com>
2024-02-02 11:29:19 +0900

    UPDATED

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import pandas as pd
 
 @st.cache_data
 def load_data():
-    SEOUL_PUBLIC_API = st.secrets["api_credentials"]["SEOUL_PUBLIC_API"]
-    st.write(SEOUL_PUBLIC_API)
-    URL = f'http://openapi.seoul.go.kr:8088/{SEOUL_PUBLIC_API}/json/tbLnOpendataRtmsV/1/100/'
+    SERVICE_KEY = st.secrets["api_credentials"]["SERVICE_KEY"]
+    st.write(SERVICE_KEY)
+    URL = f'http://openapi.seoul.go.kr:8088/{SERVICE_KEY}/json/tbLnOpendataRtmsV/1/100/'
     st.write(URL)
     content = requests.get(URL).json()
     data = pd.DataFrame(content['tbLnOpendataRtmsV']['row'])
